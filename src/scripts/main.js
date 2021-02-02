2731d44de38648f1038f2c100b6bbf028acdd688
Dima Bond <neverpooh@gmail.com>
2021-02-02 16:39:42 +0200

    burger menu reworked

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,21 @@
 'use strict';
+
+const navSlide = () => {
+  const burger = document.querySelector('.burger');
+  const nav = document.querySelector('.burger__menu');
+  const links = document.querySelectorAll('.burger__link');
+
+  burger.addEventListener('click', () => {
+    nav.classList.toggle('burger__menu--active');
+    links.forEach(link => link.classList.toggle('burger__link--active'));
+    burger.classList.toggle('rotate');
+  });
+
+  links.forEach(el => el.addEventListener('click', () => {
+    nav.classList.toggle('burger__menu--active');
+    links.forEach(link => link.classList.toggle('burger__link--active'));
+    burger.classList.toggle('rotate');
+  }));
+};
+
+navSlide();
